6269ac3cb5f7124b85678c36d5b02f1cc9703984
ashensavi <ashensavinda0@gmail.com>
2025-04-29 13:42:41 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './employee/employee-form/employee-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    export const routes: Routes = [
+        { path: '', redirectTo: 'employees', pathMatch: 'full' },
+        { path: 'employees', component: EmployeeListComponent },
+        { path: 'employees/add', component: EmployeeFormComponent },
+        { path: 'employees/edit/:id', component: EmployeeFormComponent },
+      ];
+];
